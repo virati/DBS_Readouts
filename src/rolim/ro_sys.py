c619f671176583d477efabfeb8a4f25726ea972d
Vineet Tiruvadi <virati@gmail.com>
2022-07-26 17:48:11 +0000

    more ro sys buildout

--- src/rolim/ro_sys.py
+++ src/rolim/ro_sys.py
@@ -1,19 +1,43 @@
 import jax.numpy as np
 import numpy as nnp
 
+
 def unity(x):
+    """
+    Basic unity function
+    """
     return x
 
-def consensus(x,L : np.ndarray = None):
-    if L is none:
-        L = 
+
+def statics(x):
+    return 0
+
+
+def stables(x, c=0,):
+    return -(x - c)
+
+
+def consensus(x):
+    """
+    Basic all-to-all consensus dynamics
+    """
+    N = x.shape()[-1]
+    L = np.ones(N, N)
+
     return L @ x
 
+
 class basic_system:
-    def __init__(self,dimensions = 3, elements = 2):
-        self.Gamma = unity
-        self.H = unity
-        self.x = np.zeros((elements, dimensions))
-        self.f = consensus
+    def __init__(self, B=1, M=1, regions=2, dimensions=1):
+        self.Gamma = np.ones(regions, B)
+        self.H = np.ones(regions, M)
+        self.x = np.zeros((regions, dimensions))
+        self.f = statics
+
 
 class RO_SYS(basic_system):
+    def __init__(self, **kwargs):
+        super.__init__(self, **kwargs)
+
+    def coverage(self) -> float:
+        return self.Gamma @ self.H / np.abs(self.Gamma)
