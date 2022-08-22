830681c72d8cd1fcc5b585703e676779cf6c5a5e
Vineet Tiruvadi <virati@gmail.com>
2022-08-22 04:57:39 +0000

    trascribing stats into class

--- src/dbread/ro_sys.py
+++ src/dbread/ro_sys.py
@@ -29,3 +29,26 @@ class RO_SYS(basic_system):
 
     def coverage(self) -> float:
         return np.dot(self.gamma.T, self.H) / (np.sum(self.gamma))
+
+    def gen_synth_states(self, T: int = 10_000) -> np.ndarray:
+        self.X_states = nnp.random.multivariate_normal(
+            0 * np.zeros(self.regions), nnp.eye(self.regions), size=(T,)
+        )
+
+        return self
+
+    def measure(self):
+        return nnp.dot(basic_system.H.T, self.X_states.T).squeeze()
+
+    def behave(self):
+        return nnp.dot(basic_system.gamma.T, self.X_states.T).squeeze()
+
+    def prediction_stats(self, plot=False, X=None, Y=None):
+        if plot:
+            fig = plt.figure()
+            ax = fig.add_subplot(111)
+
+            plt.scatter(X, Y)
+            pearsonr(Y, X)
+
+            model = sm.OLS(Y, X)
