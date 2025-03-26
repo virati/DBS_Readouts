eafd121fbabe84c3c7627357cd41c6698cbddbd7
Vineet Tiruvadi <vineet.tiruvadi@gmail.com>
2025-03-26 22:31:57 +0000

    weird behavior at first clip

--- notebooks/paper/random_gamma_clipping.py
+++ notebooks/paper/random_gamma_clipping.py
@@ -27,28 +27,25 @@ num_behaviors = 1
 # %%
 putative = rosys.rosys(num_nodes=num_nodes,
                        num_probes=num_probes, num_behaviors=num_probes)
-putative.set_H(innerprod, clip(np.eye(num_nodes), clip_num=2)).set_Γ(
-    innerprod, clip(np.random.normal(0, 1, size=(num_nodes, 1)), clip_num=0))
 
-
-putative.plot_graph()
-putative.plot_coverage()
-putative.plot_connectivity()
 # %%
 putative.plot_x()
 
 # %%
 H_vec_clipped = clip(np.eye(num_probes), clip_num=0)
+# Γ_vec = np.ones((num_nodes, 1))
+Γ_vec = 10*np.exp(-np.arange(num_nodes).reshape(-1, 1)) + 1
+# Γ_vec += clip(np.random.normal(0, 1, size=(num_nodes, 1)), clip_num=0)
+
 plt.imshow(H_vec_clipped)
 plt.show()
 putative.gen_synth_states(T=1000)
 putative.set_H(innerprod, H_vec_clipped).measure(plot=True)
-putative.behave(plot=True)
+putative.set_Γ(innerprod, Γ_vec).behave(plot=True)
 
 # %%
 assessment = putative.train_readout().test_readout()
 print(assessment)
-# %%
 assessment = efficacy(putative).run()
 [plot_against_Γ(putative, assessment[label], label=label)
  for label in ['accuracy', 'alignment']]
